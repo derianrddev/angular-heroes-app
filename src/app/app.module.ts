bf5b4f62e701bffd8f65eed87033ba8d003fedd1
Derian <dmrodriguez2000@gmail.com>
2024-05-03 13:42:09 -0600

    heroes list design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +15,7 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     SharedModule
   ],
   providers: [
--- src/app/heroes/pages/list-page/list-page.component.ts
+++ src/app/heroes/pages/list-page/list-page.component.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { HeroesService } from '../../services/heroes.service';
+import { Hero } from '../../interfaces/hero.interface';
 
 @Component({
   selector: 'heroes-list-page',
   templateUrl: './list-page.component.html'
 })
-export class ListPageComponent {
+export class ListPageComponent implements OnInit {
+  public heroes: Hero[] = [];
 
+  constructor(private heroesService: HeroesService) {}
+
+  ngOnInit(): void {
+    this.heroesService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  }
 }
